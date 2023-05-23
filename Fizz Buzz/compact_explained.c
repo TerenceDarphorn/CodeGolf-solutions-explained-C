b7b6aaebbf32d1c64035525b523523de5a80ed5b
TerenceDarphorn <70152333+TerenceDarphorn@users.noreply.github.com>
2023-05-23 12:47:58 +0000

    Update compact_explained.c

--- Fizz Buzz/compact_explained.c
+++ Fizz Buzz/compact_explained.c
@@ -1,9 +1,9 @@
 // (no need to include stdio.h)
 int main(){ //(no arguments needed)
-for(int i=1;i<101;i+=1){ // i < 101 is equal to i <= 100 (for integers at least)
+for(int i=1;i<101;i++){ // i < 101 is equal to i <= 100 (for integers at least) -- i++ is equal to i += 1, they increase i by 1
 if(i%15==0)printf("FizzBuzz\n"); // if i is a multiple of 3 and a multiple of 5, it must be a multiple of 15 and checking for that is shorter
 else if(i%3==0)printf("Fizz\n"); // when there is only one statement in the if (or else), the braces {} can be omitted.
-else if(i % 5 == 0)printf("Buzz\n");
+else if(i%5==0)printf("Buzz\n");
 else printf("%i\n",i);
 } //end of for loop
 //(no need to return)
